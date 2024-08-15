5c764bc39a9a56e9380007f84c2c66aa1828bb61
Homework-DAD <Homework_DAD@outlook.com>
2024-08-15 16:35:52 +0800

    提交第三次采集任务
    好不容易才凑够了15个脚本，还有另外3个脚本未解决。
    
    其他网站因无法访问未创建脚本
    
    详情见下面腾讯文档链接，”肖宇“负责的网站的备注
    
    https://docs.qq.com/sheet/DS2tucFljZ3JHUWVK

---
+++ plugins/dev/1715.go
@@ -0,0 +1,40 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1715", "菲华网", "https://www.phhua.com/")
+
+	engine.SetStartingURLs([]string{"https://www.phhua.com/news/"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".Scont-bd > li > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Visit(element.Attr("href"), crawlers.News)
+	})
+
+	engine.OnHTML(".bk-article-body", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+
+	engine.OnHTML(".Scont-bd > div > a:nth-last-child(2)", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Visit(element.Attr("href"), crawlers.Index)
+	})
+
+}
