ef4d46bc4eacbc881ac7476485c4f45d2c94d05f
Terminal017 <149765385+Terminal017@users.noreply.github.com>
2024-08-24 23:26:21 +0800

    Create 1410.go
    存在人机验证

---
+++ plugins/errors/1410.go
@@ -0,0 +1,36 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1410", "外交部", "https://dfa.gov.ph/")
+
+	engine.SetStartingURLs([]string{"https://dfa.gov.ph/"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	//Error：存在人机验证
+	engine.OnHTML(".lnd_latestnews > li > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+
+	engine.OnHTML(".articleBody > P", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
