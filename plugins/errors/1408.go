bed208232290dc4affa3d50bed5cd86f535cdfd4
Terminal017 <149765385+Terminal017@users.noreply.github.com>
2024-08-24 23:25:39 +0800

    Create 1408.go

---
+++ plugins/errors/1408.go
@@ -0,0 +1,37 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1408", "住房和土地使用管理委员会", "https://dhsud.gov.ph/")
+
+	engine.SetStartingURLs([]string{"https://dhsud.gov.ph/resources/media/news-and-events/"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	//Error：存在人机验证
+	engine.OnHTML(".news-content-excerpt > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+
+	engine.OnHTML(".news_pagination wpnw-numeric > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+
+}
