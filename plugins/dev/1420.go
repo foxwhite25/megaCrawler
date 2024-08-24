c58e5e265ffb9864399f1bf0c528e408149ab941
Terminal017 <149765385+Terminal017@users.noreply.github.com>
2024-08-25 00:29:59 +0800

    Create 1420.go

---
+++ plugins/dev/1420.go
@@ -0,0 +1,44 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1420", "外交部", "https://www.mofa.gov.vn/")
+
+	engine.SetStartingURLs([]string{"https://www.mofa.gov.vn/tt_baochi/pbnfn"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".documentContent > div > div > div > div > table > tbody > tr> td > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+
+	engine.OnHTML(`#region-content > div > div > div > div > div > a`, func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+
+	engine.OnHTML(`#region-content > div > font > b > span`, func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Description += element.Text
+	})
+
+	engine.OnHTML("div.plain > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+
+}
