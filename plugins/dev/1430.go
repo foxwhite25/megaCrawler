4a40a2ca2b9e40019d290ba73f58d2878a03ef0e
TS <149765385+Terminal017@users.noreply.github.com>
2024-09-27 23:44:06 +0800

    Update 1430.go
    学长，这个网站完整的sitemap非常混乱（而且非常big），索引页和新闻页混在一起（并且新闻页还没有news的标签），我尝试使用正则表达式进行匹配，看一下能不能解决问题。

--- plugins/dev/1430.go
+++ plugins/dev/1430.go
@@ -1,8 +1,10 @@
 package dev
 
 import (
+	"fmt"
 	"megaCrawler/crawlers"
 	"megaCrawler/extractors"
+	"regexp"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -10,7 +12,17 @@ import (
 func init() {
 	engine := crawlers.Register("1430", "新加坡商业时报", "https://www.businesstimes.com.sg/")
 
-	engine.SetStartingURLs([]string{"https://www.businesstimes.com.sg/sitemap.xml"})
+	// 创建一个切片来存储所有的sitemap URL
+	startingURLs := []string{}
+
+	// 使用循环来生成多个sitemap的URL
+	for i := 1; i <= 84; i++ {
+		url := fmt.Sprintf("https://www.businesstimes.com.sg/_plat/api/v1/sitemap.xml?page=%d", i)
+		startingURLs = append(startingURLs, url) // 将生成的 URL 添加到切片
+	}
+
+	// 设置起始URL
+	engine.SetStartingURLs(startingURLs)
 
 	extractorConfig := extractors.Config{
 		Author:       true,
@@ -25,8 +37,15 @@ func init() {
 
 	extractorConfig.Apply(engine)
 
+	// 这是匹配新闻的正则表达式
+	newsURLPattern := regexp.MustCompile(`^https://www\.businesstimes\.com\.sg/[a-zA-Z0-9/-]+$`)
+
 	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Text, crawlers.News)
+		url := element.Text
+		// 只在满足表达式的情况下进行匹配
+		if newsURLPattern.MatchString(url) {
+			engine.Visit(url, crawlers.News)
+		}
 	})
 
 	engine.OnHTML(".container.px-0.mx-auto.w-full.mb-4 > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
