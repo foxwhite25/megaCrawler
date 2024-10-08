6bc4bf0f1ee567bab1e4f447d4a1580b1749532b
TS <149765385+Terminal017@users.noreply.github.com>
2024-10-08 20:51:53 +0800

    Create 1437.go

---
+++ plugins/dev/1437.go
@@ -0,0 +1,39 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1437", "Think China", "https://www.thinkchina.sg/")
+
+	engine.SetStartingURLs([]string{"https://www.thinkchina.sg/_plat/api/sitemap"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         true,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Text, crawlers.News)
+	})
+
+	engine.OnHTML("._articleBlurb_1edd3_1 > span", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Description += element.Text
+	})
+
+	engine.OnHTML("._paragraph_10opw_1._larger_10opw_42 > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
