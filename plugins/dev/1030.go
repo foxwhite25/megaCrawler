cd7970af8dfc32c38e354ba2ca41a7ab435d916b
Fox_white <vct.xie@gmail.com>
2024-10-15 00:55:27 +0800

    fix: 1030

--- plugins/dev/1030.go
+++ plugins/dev/1030.go
@@ -1,16 +1,16 @@
 package dev
 
 import (
+	"github.com/gocolly/colly/v2"
 	"megaCrawler/crawlers"
 	"megaCrawler/extractors"
-
-	"github.com/gocolly/colly/v2"
+	"strings"
 )
 
 func init() {
 	engine := crawlers.Register("1030", "东西方中心East_West Center", "https://www.eastwestcenter.org/")
 
-	engine.SetStartingURLs([]string{"https://www.eastwestcenter.org/"})
+	engine.SetStartingURLs([]string{"https://www.eastwestcenter.org/sitemap.xml"})
 
 	extractorConfig := extractors.Config{
 		Author:       true,
@@ -24,27 +24,13 @@ func init() {
 	}
 
 	extractorConfig.Apply(engine)
-	// engine.OnResponse((func(response *colly.Response, ctx *crawlers.Context) {
-	// 	crawlers.Sugar.Debugln(response.StatusCode)
-	// 	crawlers.Sugar.Debugln(string(response.Body))
-	// }))
-	engine.OnHTML(".link--primary", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.Index)
-	})
-	engine.OnHTML(".pager__item pager__item--next > a ", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.Index)
-	})
-	engine.OnHTML(".teaser__image-link", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.News)
-	})
-	engine.OnHTML(".fullpage__wysiwyg-body  > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		ctx.Content += element.Text
-	})
-	// engine.OnHTML(".pager-next > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
 
-	// engine.OnHTML(".pg-normal pg-button pg-next-button", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		if strings.Contains(element.Text, "/publication/") {
+			engine.Visit(element.Text, crawlers.News)
+		}
+		if strings.Contains(element.Text, "sitemap") {
+			engine.Visit(element.Text, crawlers.Index)
+		}
+	})
 }
