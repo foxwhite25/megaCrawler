b22d4fb6b418e89839a61bc852fbb749b6cfc3a4
Fox_white <vct.xie@gmail.com>
2024-10-15 00:26:54 +0800

    fix: 1026

--- plugins/dev/1026.go
+++ plugins/dev/1026.go
@@ -8,9 +8,9 @@ import (
 )
 
 func init() {
-	engine := crawlers.Register("1026", "民主中心", "https://democracy.uchicago.edu/news/")
+	engine := crawlers.Register("1026", "民主中心", "https://democracy.uchicago.edu/")
 
-	engine.SetStartingURLs([]string{"https://democracy.uchicago.edu/news/"})
+	engine.SetStartingURLs([]string{"https://democracy.uchicago.edu/wp-sitemap-posts-post-1.xml"})
 
 	extractorConfig := extractors.Config{
 		Author:       true,
@@ -24,27 +24,12 @@ func init() {
 	}
 
 	extractorConfig.Apply(engine)
-	// engine.OnResponse((func(response *colly.Response, ctx *crawlers.Context) {
-	// 	crawlers.Sugar.Debugln(response.StatusCode)
-	// 	crawlers.Sugar.Debugln(string(response.Body))
-	// }))
-	engine.OnHTML(".post-content >  a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.Index)
-	})
-	engine.OnHTML(".entry-content > p > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.News)
-	})
-	// engine.OnHTML(".sitemap > tbody > tr > td > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.News)
-	// })
-	// engine.OnHTML(".post-title > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Report)
-	// })
-	engine.OnHTML(".article__abs > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		ctx.Content += element.Text
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Text, crawlers.News)
 	})
 
-	// engine.OnHTML(".pg-normal pg-button pg-next-button", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
+	engine.OnHTML(".entry-content", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content = crawlers.StandardizeSpaces(element.Text)
+	})
 }
