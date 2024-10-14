d55f172386310f49849c2e9fc5ef44582f1a4521
Homework-DAD <Homework_DAD@outlook.com>
2024-10-15 02:43:42 +0800

    feat: plugins 1739-1748 (#128)
    * 第一批脚本
    
    1741和1748由于网络问题无法判断是否有效。
    
    * feat: reverse engineer 1748 api
    
    ---------
    
    Co-authored-by: Fox_white <vct.xie@gmail.com>

---
+++ plugins/dev/1739.go
@@ -0,0 +1,35 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1739", "波音韩国工程技术中心", "https://www.boeing.co.kr/")
+
+	engine.SetStartingURLs([]string{"https://www.boeing.co.kr/news/"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".news-item-link", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+
+	engine.OnHTML(".text", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
