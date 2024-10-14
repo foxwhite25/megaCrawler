dd6f44246cafca8ba951479b28e25e0b73415ec3
Fox_white <vct.xie@gmail.com>
2024-10-15 01:06:01 +0800

    fix: 1047

--- plugins/dev/1047.go
+++ plugins/dev/1047.go
@@ -10,7 +10,7 @@ import (
 func init() {
 	engine := crawlers.Register("1047", "美国国家科学基金会", "https://new.nsf.gov/news/releases")
 
-	engine.SetStartingURLs([]string{"https://www.nsf.gov/news/releases"})
+	engine.SetStartingURLs([]string{"https://new.nsf.gov/news/releases"})
 
 	extractorConfig := extractors.Config{
 		Author:       true,
@@ -24,31 +24,12 @@ func init() {
 	}
 
 	extractorConfig.Apply(engine)
-	// engine.OnResponse((func(response *colly.Response, ctx *crawlers.Context) {
-	// 	crawlers.Sugar.Debugln(response.StatusCode)
-	// 	crawlers.Sugar.Debugln(string(response.Body))
-	// }))
 
-	// engine.OnHTML(".usa-nav__submenu-item > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
-	// engine.OnHTML(".region > div > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
 	engine.OnHTML(".latest-news-teaser__headline > h3 > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
 		engine.Visit(element.Attr("href"), crawlers.News)
 	})
-	engine.OnHTML(".clearfix > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		ctx.Content += element.Text
-	})
-	// engine.OnHTML(".link-to-pdf > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Report)
-	// })
+
 	engine.OnHTML(".pager__item > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
 		engine.Visit(element.Attr("href"), crawlers.Index)
 	})
-
-	// engine.OnHTML(".pg-normal pg-button pg-next-button", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
 }
