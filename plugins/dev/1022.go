9bb23075f00241964107d86b903bde5ba894346c
Fox_white <vct.xie@gmail.com>
2024-10-15 00:29:01 +0800

    fix: 1022 删了反而能采集上给我气笑了

--- plugins/dev/1022.go
+++ plugins/dev/1022.go
@@ -18,30 +18,18 @@ func init() {
 		Language:     true,
 		PublishDate:  true,
 		Tags:         true,
-		Text:         false,
+		Text:         true,
 		Title:        true,
 		TextLanguage: "",
 	}
 
 	extractorConfig.Apply(engine)
-	// engine.OnResponse((func(response *colly.Response, ctx *crawlers.Context) {
-	// 	crawlers.Sugar.Debugln(response.StatusCode)
-	// 	crawlers.Sugar.Debugln(string(response.Body))
-	// }))
-	engine.OnHTML(".primary-navigation >ul> li > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+
+	engine.OnHTML(".primary-navigation > ul> li > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
 		engine.Visit(element.Attr("href"), crawlers.Index)
 	})
-	// engine.OnHTML(".event-series__each > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
+
 	engine.OnHTML(".article > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
 		engine.Visit(element.Attr("href"), crawlers.News)
 	})
-	engine.OnHTML(".post__article__content > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		ctx.Content += element.Text
-	})
-
-	// 	engine.OnHTML(".card-grid__load-more  > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 		engine.Visit(element.Attr("href"), crawlers.Index)
-	// 	})
 }
