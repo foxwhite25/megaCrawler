c1cab3d89eb39fdcb2f6dcbf4c497e3f5469382c
Fox_white <vct.xie@gmail.com>
2024-10-14 23:48:03 +0800

    fix: 1028

---
+++ plugins/errors/1028.go
@@ -0,0 +1,51 @@
+package errors
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+// 该网站并没有内容，而是转发其他网页的新闻
+func init() {
+	engine := crawlers.Register("1028", "沃森国际公共事务研究所关于战争经费的研究", "https://watson.brown.edu/costsofwar/news")
+
+	engine.SetStartingURLs([]string{"https://watson.brown.edu/costsofwar/news"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         true,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+	// engine.OnResponse((func(response *colly.Response, ctx *crawlers.Context) {
+	// 	crawlers.Sugar.Debugln(response.StatusCode)
+	// 	crawlers.Sugar.Debugln(string(response.Body))
+	// }))
+	// engine.OnHTML(".leaf > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+	// 	engine.Visit(element.Attr("href"), crawlers.Index)
+	// })
+	engine.OnHTML(".newsitem  > div > a ", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+	// engine.OnHTML(".post-title > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+	// 	engine.Visit(element.Attr("href"), crawlers.Report)
+	// })
+	engine.OnHTML(".read__content  > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+	engine.OnHTML(".pager-next > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+
+	// engine.OnHTML(".pg-normal pg-button pg-next-button", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+	// 	engine.Visit(element.Attr("href"), crawlers.Index)
+	// })
+}
