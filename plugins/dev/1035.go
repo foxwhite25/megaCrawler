021ec94caa3dbd970cfc2f03c3bdfc19f142644f
Fox_white <vct.xie@gmail.com>
2024-10-15 00:18:13 +0800

    fix: 1035

--- plugins/dev/1035.go
+++ plugins/dev/1035.go
@@ -8,9 +8,9 @@ import (
 )
 
 func init() {
-	engine := crawlers.Register("1035", "远东研究所/中国与现代亚洲研究所", "https://www.ifes-ras.ru")
+	engine := crawlers.Register("1035", "远东研究所/中国与现代亚洲研究所", "https://www.ifes-ras.ru/")
 
-	engine.SetStartingURLs([]string{"https://www.ifes-ras.ru"})
+	engine.SetStartingURLs([]string{"https://www.iccaras.ru/novosti.html"})
 
 	extractorConfig := extractors.Config{
 		Author:       true,
@@ -18,33 +18,22 @@ func init() {
 		Language:     true,
 		PublishDate:  true,
 		Tags:         true,
-		Text:         true,
+		Text:         false,
 		Title:        true,
 		TextLanguage: "",
 	}
 
 	extractorConfig.Apply(engine)
-	// engine.OnResponse((func(response *colly.Response, ctx *crawlers.Context) {
-	// 	crawlers.Sugar.Debugln(response.StatusCode)
-	// 	crawlers.Sugar.Debugln(string(response.Body))
-	// }))
-	engine.OnHTML(".subscribe link_un", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.Index)
-	})
+
 	engine.OnHTML(".page-link", func(element *colly.HTMLElement, ctx *crawlers.Context) {
 		engine.Visit(element.Attr("href"), crawlers.Index)
 	})
+
 	engine.OnHTML(".news-link", func(element *colly.HTMLElement, ctx *crawlers.Context) {
 		engine.Visit(element.Attr("href"), crawlers.News)
 	})
+
 	engine.OnHTML(".news-text > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
 		ctx.Content += element.Text
 	})
-	// engine.OnHTML(".pager-next > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
-
-	// engine.OnHTML(".pg-normal pg-button pg-next-button", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
 }
