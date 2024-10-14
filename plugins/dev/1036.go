72a5f25cccf2fd190ddb852348378f2b7b056b8f
Fox_white <vct.xie@gmail.com>
2024-10-15 01:10:59 +0800

    fix: 1036

--- plugins/dev/1036.go
+++ plugins/dev/1036.go
@@ -3,6 +3,7 @@ package dev
 import (
 	"megaCrawler/crawlers"
 	"megaCrawler/extractors"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -10,7 +11,7 @@ import (
 func init() {
 	engine := crawlers.Register("1036", "国际调查记者联盟", "https://www.icij.org/")
 
-	engine.SetStartingURLs([]string{"https://www.icij.org/"})
+	engine.SetStartingURLs([]string{"https://www.icij.org/sitemap.xml"})
 
 	extractorConfig := extractors.Config{
 		Author:       true,
@@ -24,27 +25,12 @@ func init() {
 	}
 
 	extractorConfig.Apply(engine)
-	// engine.OnResponse((func(response *colly.Response, ctx *crawlers.Context) {
-	// 	crawlers.Sugar.Debugln(response.StatusCode)
-	// 	crawlers.Sugar.Debugln(string(response.Body))
-	// }))
-	engine.OnHTML(".nav-item-header > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.Index)
-	})
-	// engine.OnHTML(".page-link", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
-	engine.OnHTML(".article-title__title > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.News)
-	})
-	engine.OnHTML(".col-12 > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		ctx.Content += element.Text
-	})
-	// engine.OnHTML(".pager-next > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
 
-	// engine.OnHTML(".pg-normal pg-button pg-next-button", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-	// 	engine.Visit(element.Attr("href"), crawlers.Index)
-	// })
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		if strings.Contains(element.Text, "post-sitemap") {
+			engine.Visit(element.Text, crawlers.Index)
+		} else if strings.Contains(ctx.URL, "post-sitemap") {
+			engine.Visit(element.Text, crawlers.News)
+		}
+	})
 }
