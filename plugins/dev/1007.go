7d7a48cae6e55c2bf8a275a17366e98cca2a1695
Fox_white <vct.xie@gmail.com>
2024-10-14 23:31:59 +0800

    fix: 1007

--- plugins/dev/1007.go
+++ plugins/dev/1007.go
@@ -3,6 +3,7 @@ package dev
 import (
 	"megaCrawler/crawlers"
 	"megaCrawler/extractors"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -10,7 +11,7 @@ import (
 func init() {
 	engine := crawlers.Register("1007", "SMNI NEWS CHANNEL", "https://smninewschannel.com/")
 
-	engine.SetStartingURLs([]string{"https://smninewschannel.com/"})
+	engine.SetStartingURLs([]string{"https://smninewschannel.com/sitemap_index.xml"})
 
 	extractorConfig := extractors.Config{
 		Author:       true,
@@ -24,20 +25,16 @@ func init() {
 	}
 
 	extractorConfig.Apply(engine)
-	// engine.OnResponse((func(response *colly.Response, ctx *crawlers.Context) {
-	// 	crawlers.Sugar.Debugln(response.StatusCode)
-	// 	crawlers.Sugar.Debugln(string(response.Body))
-	// }))
-	engine.OnHTML(".nav-menu > li > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.Index)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		if strings.Contains(element.Text, "post-sitemap") {
+			engine.Visit(element.Text, crawlers.Index)
+		} else if strings.Contains(ctx.URL, "post-sitemap") {
+			engine.Visit(element.Text, crawlers.News)
+		}
 	})
-	engine.OnHTML(".entry-title > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.News)
-	})
+
 	engine.OnHTML(".entry-content > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
 		ctx.Content += element.Text
-	})
-	engine.OnHTML(".next", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.Index)
 	})
 }
