5d005ed2cee235ac19aef3973607120a295c30c2
TS <149765385+Terminal017@users.noreply.github.com>
2024-10-19 20:10:42 +0800

    feat: plugins 1444-1461 (#134)

---
+++ plugins/storage/1456.go
@@ -0,0 +1,62 @@
+package storage
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	//日本的这几个网站非常相似，后面的部分部门隶属这个农林水产省
+	engine := crawlers.Register("1456", "农林水产省", "http://www.maff.go.jp/")
+
+	engine.SetStartingURLs([]string{
+		"https://www.maff.go.jp/j/press/arc/index.html",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".mgbk > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+
+	engine.OnHTML(".list_item > dd > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.News)
+	})
+
+	//采集PDF
+	engine.OnHTML(".content > p > a, .content > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		fileURL := element.Attr("href")
+		if strings.Contains(fileURL, ".pdf") {
+			url, err := element.Request.URL.Parse(element.Attr("href"))
+			if err != nil {
+				crawlers.Sugar.Error(err.Error())
+				return
+			}
+			ctx.File = append(ctx.File, url.String())
+			ctx.PageType = crawlers.Report
+		}
+	})
+
+	engine.OnHTML(".content > p:not([class]),.content > h2", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
