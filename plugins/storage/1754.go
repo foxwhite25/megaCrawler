2217cdf1da0f41a071849b04b124625d22c873fd
Homework-DAD <Homework_DAD@outlook.com>
2024-10-19 20:51:52 +0800

    feat: plugins 1749-1760 (#132)
    由于1741和1748未通过，这次上传12个脚本。
    1759部分链接为视频（空内容）。
    
    Co-authored-by: Fox_white <39846845+foxwhite25@users.noreply.github.com>

---
+++ plugins/storage/1754.go
@@ -0,0 +1,42 @@
+package storage
+
+import (
+	"strings"
+
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1754", "罗恩保罗研究所", "https://ronpaulinstitute.org/")
+
+	engine.SetStartingURLs([]string{"https://ronpaulinstitute.org/sitemaps.xml"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		switch {
+		case strings.Contains(element.Text, "post-sitemap"):
+			engine.Visit(element.Text, crawlers.Index)
+		default:
+			engine.Visit(element.Text, crawlers.News)
+		}
+	})
+
+	engine.OnHTML(".et_pb_with_border.et_pb_column_3_4", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
