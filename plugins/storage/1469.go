171a29a224972d12e84258182a8bf5bd78f1a8c4
TS <149765385+Terminal017@users.noreply.github.com>
2024-10-26 01:01:21 +0800

    feat: 1462-1473 (#136)
    提交十月第三周的网站，总共12个

---
+++ plugins/storage/1469.go
@@ -0,0 +1,41 @@
+package storage
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1469", "澳大利亚税务局", "https://www.ato.gov.au")
+
+	engine.SetStartingURLs([]string{
+		"https://www.ato.gov.au/sitemap.xml",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		if strings.Contains(element.Text, "media-centre") {
+			engine.Visit(element.Text, crawlers.News)
+		}
+	})
+
+	engine.OnHTML(".AtoContentWrapper_rich-text-content__HY8CB > div > p, .AtoContentWrapper_rich-text-content__HY8CB > div > h2",
+		func(element *colly.HTMLElement, ctx *crawlers.Context) {
+			ctx.Content += element.Text
+		})
+}
---
+++ plugins/storage/1471.go
@@ -0,0 +1,64 @@
+package storage
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1471", "澳大利亚基础设施与地区发展部", "https://www.infrastructure.gov.au/")
+
+	engine.SetStartingURLs([]string{
+		"https://www.infrastructure.gov.au/sitemap.xml?page=1",
+		"https://www.infrastructure.gov.au/sitemap.xml?page=2",
+		"https://www.infrastructure.gov.au/sitemap.xml?page=3",
+		"https://www.infrastructure.gov.au/sitemap.xml?page=4",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	//这里NEWS一般是纯新闻，其他是媒体中心的一些报告，通常包含PDF
+	keywords := []string{"/media/", "/news/", "/media-centre/"}
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		for _, keyword := range keywords {
+			if strings.Contains(element.Text, keyword) {
+				engine.Visit(element.Text, crawlers.News)
+				return
+			}
+		}
+	})
+
+	//采集PDF
+	engine.OnHTML(".filesize > a, .file > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		fileURL := element.Attr("href")
+		if strings.Contains(fileURL, ".pdf") {
+			url, err := element.Request.URL.Parse(element.Attr("href"))
+			if err != nil {
+				crawlers.Sugar.Error(err.Error())
+				return
+			}
+			ctx.File = append(ctx.File, url.String())
+			ctx.PageType = crawlers.Report
+		}
+	})
+
+	engine.OnHTML(".my-5 > p, .my-5 > ul, .pb-5 > p, .node__content.clearfix > div > p,.node__content.clearfix > div > ul",
+		func(element *colly.HTMLElement, ctx *crawlers.Context) {
+			ctx.Content += element.Text
+		})
+
+}
