07a7436880d852e7210faf97aefd9b474562bde9
GUshouCHUANwei <163318722+GUshouCHUANwei@users.noreply.github.com>
2024-11-01 20:31:48 +0800

    十一月第一周
    十一月第一批，拢共十个

---
+++ plugins/dev/1098.go
@@ -0,0 +1,40 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1098", "政治家报", "http://www.thestatesman.com/")
+
+	engine.SetStartingURLs([]string{"https://www.thestatesman.com/sitemap.xml"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         true,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		switch {
+
+		case strings.Contains(element.Text, "sitemap"):
+			engine.Visit(element.Text, crawlers.Index)
+
+		default:
+			engine.Visit(element.Text, crawlers.News)
+		}
+	})
+
+}
