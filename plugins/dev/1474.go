ae75aa9048df734d13544821c2389d8cf8d849e6
TS <149765385+Terminal017@users.noreply.github.com>
2024-11-01 19:30:13 +0800

    添加网站
    添加10月第4周的网站，共12个

---
+++ plugins/dev/1474.go
@@ -0,0 +1,50 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("E470", "澳大利亚总检察院", "https://www.ag.gov.au/")
+
+	engine.SetStartingURLs([]string{"https://www.ag.gov.au/sitemap.xml?page=2"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         true,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Text, crawlers.News)
+	})
+
+	//采集PDF
+	engine.OnHTML(".file.file--mime-application-pdf > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		fileURL := element.Attr("href")
+		if strings.Contains(fileURL, ".pdf") {
+			url, err := element.Request.URL.Parse(element.Attr("href"))
+			if err != nil {
+				crawlers.Sugar.Error(err.Error())
+				return
+			}
+			ctx.File = append(ctx.File, url.String())
+			ctx.PageType = crawlers.Report
+		}
+	})
+
+	engine.OnHTML(".block-region-content", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
---
+++ plugins/dev/1477.go
@@ -0,0 +1,64 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1477", "澳大利亚卫生部", "https://www.health.gov.au/")
+
+	engine.SetStartingURLs([]string{"https://www.health.gov.au/news"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML("h3.au-display-md > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.News)
+	})
+
+	engine.OnHTML("li.pager__item > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.Index)
+	})
+
+	engine.OnHTML("a.health-file__link", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		fileURL := element.Attr("href")
+		if strings.Contains(fileURL, ".pdf") {
+			url, err := element.Request.URL.Parse(element.Attr("href"))
+			if err != nil {
+				crawlers.Sugar.Error(err.Error())
+				return
+			}
+			ctx.File = append(ctx.File, url.String())
+			ctx.PageType = crawlers.Report
+		}
+	})
+
+	engine.OnHTML(".node.node--h_news_article.node--full > div > p,.node.node--h_news_article.node--full > div > ul",
+		func(element *colly.HTMLElement, ctx *crawlers.Context) {
+			ctx.Content += element.Text
+		})
+}
