0f5534c80f6fc15b7151a8b93b2cec8a44fb3167
Homework-DAD <Homework_DAD@outlook.com>
2024-11-03 11:22:25 +0800

    feat: plugins 1774-1784 (#143)
    * 十一月第一周
    
    * chore: dev -> stage
    
    ---------
    
    Co-authored-by: Fox_white <vct.xie@gmail.com>

---
+++ plugins/storage/1775.go
@@ -0,0 +1,42 @@
+package storage
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1775", "Manchester Evening News", "https://www.manchestereveningnews.co.uk/")
+
+	engine.SetStartingURLs([]string{
+		"https://www.scottishdailyexpress.co.uk/news/uk-news/",
+		"https://www.manchestereveningnews.co.uk/news/greater-manchester-news/",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".headline", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Visit(element.Attr("href"), crawlers.News)
+	})
+
+	engine.OnHTML(".content-column", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+
+	engine.OnHTML(".pagi-next > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Visit(element.Attr("href"), crawlers.Index)
+	})
+}
---
+++ plugins/storage/1778.go
@@ -0,0 +1,44 @@
+package storage
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1778", "Evening Gazette", "https://www.gazettelive.co.uk/")
+
+	engine.SetStartingURLs([]string{
+		"https://www.gazettelive.co.uk/news/teesside-news/",
+		"https://www.gazettelive.co.uk/all-about/hartlepool",
+		"https://www.gazettelive.co.uk/all-about/darlington",
+		"https://www.gazettelive.co.uk/news/uk-world-news/",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".headline", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Visit(element.Attr("href"), crawlers.News)
+	})
+
+	engine.OnHTML(".article-body", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+
+	engine.OnHTML(".pagi-next > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Visit(element.Attr("href"), crawlers.Index)
+	})
+}
