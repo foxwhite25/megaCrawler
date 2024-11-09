d04216f436260fff263a6f86342dbc08a8d0af8c
TS <149765385+Terminal017@users.noreply.github.com>
2024-11-09 15:15:55 +0800

    feat: plugins 1486-1495 (#146)
    添加11月第1周的网站，共12个

---
+++ plugins/dev/1486.go
@@ -0,0 +1,60 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1486", "数码文化传媒及体育部", "https://www.gov.uk/government/organisations/department-for-culture-media-and-sport")
+
+	engine.SetStartingURLs([]string{
+		"https://www.gov.uk/search/news-and-communications?organisations[]=department-for-culture-media-and-sport&parent=department-for-culture-media-and-sport",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".gem-c-document-list__item-title > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.News)
+	})
+
+	engine.OnHTML(".govuk-pagination__next > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.Index)
+	})
+
+	engine.OnHTML("dl.gem-c-metadata__list > dd:nth-child(4)", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.PublicationTime = strings.TrimSpace(element.Text)
+	})
+
+	engine.OnHTML("p.gem-c-lead-paragraph", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Description += element.Text
+	})
+
+	engine.OnHTML(".govspeak", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
