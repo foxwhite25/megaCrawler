cb6d681a1aef7c699a922ec27e6fd75248a4115f
Homework-DAD <Homework_DAD@outlook.com>
2024-11-30 10:54:22 +0800

    feat: plugins 1804-1823 (#152)
    * 十月第三周
    
    1813采集较慢（会超时）。
    
    * 十月第四周
    
    1819和1820网络问题暂时无法判断有效。
    
    * fix: general fixes
    
    ---------
    
    Co-authored-by: Fox_white <vct.xie@gmail.com>

---
+++ plugins/dev/1809.go
@@ -0,0 +1,38 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1809", "Fairtrade Foundation", "https://www.fairtrade.org.uk/")
+
+	engine.SetStartingURLs([]string{
+		"https://www.fairtrade.org.uk/post-sitemap.xml",
+		"https://www.fairtrade.org.uk/press-releases-cpt-sitemap.xml",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Text, crawlers.News)
+	})
+
+	engine.OnHTML(".entry-content", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
