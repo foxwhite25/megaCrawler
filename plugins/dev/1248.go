63892017d314e4d412546c14359ac0040a521986
GUshouCHUANwei <163318722+GUshouCHUANwei@users.noreply.github.com>
2024-12-07 07:49:15 +0800

    12月第一周
    1244出现网络问题，无法测试

---
+++ plugins/dev/1248.go
@@ -0,0 +1,40 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1248", "THE LID", "https://lidblog.com/")
+
+	engine.SetStartingURLs([]string{
+		"https://lidblog.com/category/us-news/",
+		"https://lidblog.com/category/politics/",
+		"https://lidblog.com/category/culture/",
+		"https://lidblog.com/category/econ/",
+		"https://lidblog.com/category/faith-morality/",
+		"https://lidblog.com/category/israel/",
+		"https://lidblog.com/category/foreign-policy/"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         true,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+	engine.OnHTML(".entry-title > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+	engine.OnHTML(".alignleft > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+}
