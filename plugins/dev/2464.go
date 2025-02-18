432244ac63ed49fd2a086ee2bd30f783b5eacc9b
TS <149765385+Terminal017@users.noreply.github.com>
2025-02-19 05:58:26 +0800

    feat: plugins 2462-2571 #163
    * 一月 第1周

---
+++ plugins/dev/2464.go
@@ -0,0 +1,55 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("2464", "Across Arizona Patch", "https://patch.com/")
+
+	//这个网站新闻按地区划分，且存在重复，这里选取一部分
+	engine.SetStartingURLs([]string{
+		"https://patch.com/new-jersey/southorange?page=2",
+		"https://patch.com/new-jersey/summit?page=2",
+		"https://patch.com/new-jersey/westfield?page=2",
+		"https://patch.com/new-jersey/scotchplains?page=2",
+		"https://patch.com/new-jersey/ridgewood?page=2",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".MuiBox-root.css-1yuhvjn > div.MuiBox-root.css-0 > a.MuiTypography-root", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.News)
+	})
+
+	engine.OnHTML("div.MuiBox-root.css-zdpt2t > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+
+	engine.OnHTML("div.MuiBox-root.css-79elbk > img", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Image = []string{element.Attr("src")}
+	})
+
+	engine.OnHTML("div.styles_HTMLContent__LDG2k > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
---
+++ plugins/dev/2466.go
@@ -0,0 +1,58 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	//这个网站的新闻是对于设计作品的介绍
+	engine := crawlers.Register("2466", "Ads of the World", "https://www.adsoftheworld.com/")
+
+	engine.SetStartingURLs([]string{
+		"https://www.adsoftheworld.com/professional",
+		"https://www.adsoftheworld.com/student",
+		"https://www.adsoftheworld.com/highlighted",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML("div.overflow-hidden > div > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.News)
+	})
+
+	engine.OnHTML(`span.page > a[rel="next"]`, func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.Index)
+	})
+
+	engine.OnHTML("div.overflow-hidden > img", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Image = []string{element.Attr("src")}
+	})
+
+	engine.OnHTML("div.mb-4.whitespace-pre-line.flex.flex-col.gap-4 > p, .col-span-2 > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
