e48b12692bc33b42061fe1f23c645a77b3237669
GUshouCHUANwei <163318722+GUshouCHUANwei@users.noreply.github.com>
2025-02-19 05:56:34 +0800

    feat: plugins 1241-1260 #160
    * 12月第一周
    
    * 12月第二周

---
+++ plugins/dev/1259.go
@@ -0,0 +1,33 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1259", "华盛顿能源报告", "https://www.troutmanenergyreport.com/")
+
+	engine.SetStartingURLs([]string{"https://www.troutmanenergyreport.com/"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         true,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+	engine.OnHTML(".lxb_af-template_tags-get_linked_post_title-link", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+	engine.OnHTML("#lxb_af-loop > nav > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+}
