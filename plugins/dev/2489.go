f637ecd0681dd2ed79da88c5748c9c7537c43280
Star Trail <1404256812@qq.com>
2025-03-24 18:46:17 +0800

    Update 2489.go

--- plugins/dev/2489.go
+++ plugins/dev/2489.go
@@ -4,7 +4,6 @@ import (
 	"megaCrawler/crawlers"
 	"megaCrawler/extractors"
 	"strings"
-	"time"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -12,9 +11,23 @@ import (
 func init() {
 	engine := crawlers.Register("2489", "AutomoBlog", "https://www.automoblog.com/")
 
-	engine.SetTimeout(60 * time.Second)
-	
-	engine.SetStartingURLs([]string{"https://www.automoblog.com/sitemap_index.xml"})
+	// engine.SetTimeout(60 * time.Second)
+
+	engine.SetStartingURLs([]string{
+		"https://www.automoblog.com/post-sitemap1.xml",
+		"https://www.automoblog.com/post-sitemap5.xml",
+		"https://www.automoblog.com/post-sitemap6.xml",
+		"https://www.automoblog.com/post-sitemap7.xml",
+		"https://www.automoblog.com/post-sitemap8.xml",
+		"https://www.automoblog.com/post-sitemap9.xml",
+		"https://www.automoblog.com/post-sitemap10.xml",
+		"https://www.automoblog.com/post-sitemap11.xml",
+		"https://www.automoblog.com/post-sitemap12.xml",
+		"https://www.automoblog.com/post-sitemap13.xml",
+		"https://www.automoblog.com/post-sitemap14.xml",
+		"https://www.automoblog.com/post-sitemap15.xml",
+		"https://www.automoblog.com/post-sitemap16.xml",
+	})
 
 	extractorConfig := extractors.Config{
 		Author:       true,
@@ -41,7 +54,7 @@ func init() {
 		ctx.Image = []string{element.Attr("src")}
 	})
 
-	engine.OnHTML("div.entry-content > ul,div.entry-content > p,div.entry-content > h2",
+	engine.OnHTML("div.entry-content > ul,div.entry-content > p,div.entry-content > h2, div.taxonomy-description > p",
 		func(element *colly.HTMLElement, ctx *crawlers.Context) {
 			ctx.Content += element.Text
 		})
