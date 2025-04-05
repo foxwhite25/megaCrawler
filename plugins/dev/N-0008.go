2ae9397acf97523353ef594d3e84e46e3d25a40e
Star Trail <1404256812@qq.com>
2025-04-05 20:31:01 +0800

    add 10 new files

---
+++ plugins/dev/N-0008.go
@@ -0,0 +1,49 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("N-0008", "Vietnam Television", "https://vtv.vn/")
+
+	engine.SetStartingURLs([]string{"https://vtv.vn/tim-kiem.htm?keywords=VTV"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".mgt20 > li > h4 > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.News)
+	})
+
+	engine.OnHTML("div.page_right.mgr15 > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.Index)
+	})
+
+	engine.OnHTML("#entry-body > p:not(:has(a.link-inline-content))", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
