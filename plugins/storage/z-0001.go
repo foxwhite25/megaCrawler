93fd446207038ade4f05a323016fc3fc0505a777
GUshouCHUANwei <163318722+GUshouCHUANwei@users.noreply.github.com>
2025-04-06 11:25:52 +0800

    1

--- plugins/storage/z-0001.go
+++ plugins/storage/z-0001.go
@@ -3,14 +3,15 @@ package dev
 import (
 	"megaCrawler/crawlers"
 	"megaCrawler/extractors"
+	"net/http"
+	"strconv"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly/v2"
 )
 
 func init() {
 	engine := crawlers.Register("z-0001", "MANILA", "https://www.therakyatpost.com/")
-
-	engine.SetStartingURLs([]string{"https://www.therakyatpost.com/category/news/"})
 
 	extractorConfig := extractors.Config{
 		Author:       true,
@@ -24,8 +25,40 @@ func init() {
 	}
 
 	extractorConfig.Apply(engine)
-	engine.OnHTML(".post-inner-content > div > h5 > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
-		engine.Visit(element.Attr("href"), crawlers.News)
+	engine.OnLaunch(func() {
+		baseURL := "https://www.therakyatpost.com/category/news/page/"
+		for i := 0; true; i++ {
+			if engine.Test != nil && engine.Test.Done {
+				return
+			}
+
+			pageURL := baseURL + strconv.Itoa(i)
+			resp, err := http.Get(pageURL)
+			if err != nil {
+				continue
+			}
+
+			dom, err := goquery.NewDocumentFromReader(resp.Body)
+			if err != nil {
+				continue
+			}
+			urls := dom.Find(".post-inner-content > div > h5 > a")
+			if len(urls.Nodes) == 0 {
+				break
+			}
+			urls.Each(func(i int, selection *goquery.Selection) {
+				pageURL, ok := selection.Attr("href")
+				if !ok {
+					return
+				}
+				engine.Visit(pageURL, crawlers.News)
+			})
+
+			err = resp.Body.Close()
+			if err != nil {
+				continue
+			}
+		}
 	})
 	engine.OnHTML(".entry-content > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
 		ctx.Content += element.Text
