4a9e62539a7ba8dbd3749a1f1500cda7df152fdc
hey_bro <163318722+GUshouCHUANwei@users.noreply.github.com>
2025-04-06 12:30:45 +0800

    Apirl 1 (#179)
    * 12月第一周
    
    1244出现网络问题，无法测试
    
    * Delete plugins/dev/1242.go
    
    * Delete plugins/dev/1241.go
    
    * Delete plugins/dev/1243.go
    
    * Delete plugins/dev/1244.go
    
    * Delete plugins/dev/1245.go
    
    * Delete plugins/dev/1246.go
    
    * Delete plugins/dev/1247.go
    
    * Delete plugins/dev/1248.go
    
    * Delete plugins/dev/1249.go
    
    * Delete plugins/dev/1250.go
    
    * 四月第一周
    
    提交新的
    
    * 1

---
+++ plugins/storage/z-0008.go
@@ -0,0 +1,49 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("z-0008", "南亚监视器", "https://southasiamonitor.org/")
+
+	engine.SetStartingURLs([]string{
+		"https://southasiamonitor.org/spotlight",
+		"https://southasiamonitor.org/perspective",
+		"https://southasiamonitor.org/south-asia-abroad",
+		"https://southasiamonitor.org/indo-pacific-china-watch"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  false,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+	engine.OnHTML(".content-inner > h3 >a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+	engine.OnHTML(".pager__item--next > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.Index)
+	})
+	engine.OnHTML("span.wrap-date-location", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.PublicationTime = strings.TrimSpace(element.Text)
+	})
+	engine.OnHTML("div.news-detail >p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
