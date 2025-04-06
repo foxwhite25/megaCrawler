5f4e8303dce18236a0b0cc156647fad40968dfde
Star Trail <1404256812@qq.com>
2025-04-06 13:02:22 +0800

    storage -> prod

---
+++ plugins/production/1279.go
@@ -0,0 +1,36 @@
+﻿package production
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("1279", "停止造假", "https://www.stopfake.org/ru/glavnaya-2/")
+
+	engine.SetStartingURLs([]string{"https://www.stopfake.org/ru/category/novosti/"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+	engine.OnHTML(".entry-title > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+	engine.OnHTML(".page-nav > a:last-of-type", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+	engine.OnHTML(".td-post-content > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
---
+++ plugins/production/X0009.go
@@ -0,0 +1,42 @@
+package production
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("X0009", "东盟论坛报", "https://www.aseantribune.com/")
+
+	engine.SetStartingURLs([]string{"https://www.aseantribune.com/wp-sitemap.xml"})
+
+	engine.SetParallelism(2)
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		if strings.Contains(element.Text, "posts-post") {
+			engine.Visit(element.Text, crawlers.Index)
+		} else if !strings.Contains(element.Text, ".xml") {
+			engine.Visit(element.Text, crawlers.News)
+		}
+	})
+
+	engine.OnHTML(".elementor-element-0135340 > div > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
