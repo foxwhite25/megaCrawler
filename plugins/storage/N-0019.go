6a14b1e8f6cf33240b6d8cba844f6450a8b3e63a
TS <149765385+Terminal017@users.noreply.github.com>
2025-04-12 20:07:15 +0800

    2025.4_N2 (#184)
    add files, delete repetition

---
+++ plugins/storage/N-0019.go
@@ -0,0 +1,68 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"regexp"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("N-0019", "农林水产省", "http://www.maff.go.jp/")
+
+	engine.SetStartingURLs([]string{
+		"https://www.maff.go.jp/j/press/arc/index.html",
+	})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".mgbk > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+
+	engine.OnHTML(".list_item > dd > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.News)
+	})
+
+	engine.OnHTML(".content > p > a, .content > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		fileURL := element.Attr("href")
+		if strings.Contains(fileURL, ".pdf") {
+			url, err := element.Request.URL.Parse(element.Attr("href"))
+			if err != nil {
+				crawlers.Sugar.Error(err.Error())
+				return
+			}
+			ctx.File = append(ctx.File, url.String())
+			ctx.PageType = crawlers.Report
+		}
+	})
+
+	engine.OnHTML("div.content_utility-date", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		rawText := strings.TrimSpace(element.Text)
+		re := regexp.MustCompile(`[\p{Han}]{2}\d+年\d+月\d+日`)
+		match := re.FindString(rawText)
+		ctx.PublicationTime = match
+	})
+
+	engine.OnHTML(".content > p:not([class]),.content > h2", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
