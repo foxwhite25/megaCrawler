a5cdace1d8f8bfb190316aaec4cf18d9dac088d0
Homework-DAD <Homework_DAD@outlook.com>
2025-05-04 17:34:39 +0800

    五月第一周 (#195)

---
+++ plugins/storage/X0049.go
@@ -0,0 +1,40 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("X0049", "瓦努阿图广播电视台", "https://vbtc.vu/")
+
+	engine.SetStartingURLs([]string{"https://vbtc.vu/sitemap_index.xml"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		if strings.Contains(element.Text, "post-sitemap") {
+			engine.Visit(element.Text, crawlers.Index)
+		} else if !strings.Contains(element.Text, "xml") {
+			engine.Visit(element.Text, crawlers.News)
+		}
+	})
+
+	engine.OnHTML(".tdb-block-inner.td-fix-index > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
