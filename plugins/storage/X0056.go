d837e53953adfcbacf514900db738bd3b1845d18
Homework-DAD <Homework_DAD@outlook.com>
2025-05-10 15:09:12 +0800

    五月第二批

---
+++ plugins/storage/X0056.go
@@ -0,0 +1,41 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("X0056", "白宫", "https://www.whitehouse.gov/")
+
+	engine.SetStartingURLs([]string{"https://www.whitehouse.gov/news/"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".wp-block-post-title > a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Visit(element.Attr("href"), crawlers.News)
+	})
+
+	engine.OnHTML(".entry-content > :not(div), .field--name-body > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		element.DOM.Find("a").Remove()
+		directText := element.DOM.Text()
+		ctx.Content += directText
+	})
+
+	engine.OnHTML(".wp-block-query-pagination-numbers + a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Visit(element.Attr("href"), crawlers.Index)
+	})
+}
