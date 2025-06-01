2db5bf9cfdabe595ad68201907a5da6183247a6a
TS <149765385+Terminal017@users.noreply.github.com>
2025-06-01 10:06:10 +0800

    May 25 (#207)
    add 10 new files

---
+++ plugins/storage/N-0069.go
@@ -0,0 +1,44 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("N-0069", "Clarke County Democrat", "https://www.clarkecountydemocrat.com/")
+
+	engine.SetStartingURLs([]string{"https://www.clarkecountydemocrat.com/sitemap.xml"})
+
+	extractorConfig := extractors.Config{
+		Author:       false,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		if strings.Contains(element.Text, "/oht_article-sitemap") {
+			engine.Visit(element.Text, crawlers.Index)
+		} else if !strings.Contains(element.Text, ".xml") {
+			engine.Visit(element.Text, crawlers.News)
+		}
+	})
+
+	engine.OnHTML(`meta[name="author"]`, func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Authors = append(ctx.Authors, element.Attr("content"))
+	})
+
+	engine.OnHTML("div.entry-content > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+}
