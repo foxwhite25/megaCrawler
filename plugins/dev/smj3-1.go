61c9cc126d1a1393b18c4e8258cc5c6994e4f659
zgn-7 <2200362686@qq.com>
2025-08-19 22:15:58 +0800

    Add files via upload

---
+++ plugins/dev/smj3-1.go
@@ -0,0 +1,40 @@
+
+package dev
+
+import (
+	"time"
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+			
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("hurry3", "菲律宾游艇俱乐部", "https://philippineyachting.com/")
+	
+	engine.SetStartingURLs([]string{"https://philippineyachting.com/insights/"})
+	
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         true,
+		Title:        true,
+		TextLanguage: "",
+	}
+	
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".elementor-post__title>a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		url, err := element.Request.URL.Parse(element.Attr("href"))
+		if err != nil {
+			crawlers.Sugar.Error(err.Error())
+			return
+		}
+		engine.Visit(url.String(), crawlers.News)
+		time.Sleep(2* time.Second) 
+	})
+
+}
---
+++ plugins/dev/smj5-2.go
@@ -0,0 +1,45 @@
+
+package dev
+
+import (
+	
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("hurry5", "菲律宾海洋石油协会", "https://www.pap.ph/")
+	
+	engine.SetStartingURLs([]string{"https://www.pap.ph/announcements"})
+	
+	extractorConfig := extractors.Config{
+		Author:       false,
+		Image:        false,
+		Language:     true,
+		PublishDate:  false,
+		Tags:         true,
+		Text:         true,
+		Title:        true,
+		TextLanguage: "",
+	}
+	
+	extractorConfig.Apply(engine)
+
+	engine.OnHTML(".link.text-dark", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.News)
+	})
+
+	engine.OnHTML("li.uk-active+li>a", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Attr("href"), crawlers.Index)
+	})
+
+	engine.OnHTML(".col-md-7.py-md-5>div>div>img", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Image = append(ctx.Image, element.Attr("src"))
+	})
+
+	engine.OnHTML(".col-md-10.pt-4.text-white>font>small", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.PublicationTime += element.Text
+	})
+}
