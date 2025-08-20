5636d50ebfd04ce8e483836bf48c6f06a676ad88
Yt-ll <math_111@qq.com>
2025-08-20 21:35:11 +0800

    ZYT0820submit
    2个（补交之前3个）

---
+++ plugins/dev/ZYT0066.go
@@ -0,0 +1,26 @@
+
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+)
+
+func init() {
+	engine := crawlers.Register("ZYT0066", "DLSUuniversity", "https://www.dlsu.edu.ph")
+	
+	engine.SetStartingURLs([]string{})
+	
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        true,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         true,
+		Title:        true,
+		TextLanguage: "",
+	}
+	
+	extractorConfig.Apply(engine)
+}
