3963b17ccd47f604bee79fe728d8cb00ecc121de
Mwq90 <ma19937745896@qq.com>
2025-09-03 19:37:38 +0800

    马婉卿 (#254)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete 2825.go
    
    * Delete 2827.go
    
    * Add files via upload
    
    * Delete 2825.go
    
    * Delete 2827.go
    
    * Add files via upload
    
    * Delete 2825.go
    
    * Add files via upload
    
    ---------
    
    Co-authored-by: hey_bro <163318722+GUshouCHUANwei@users.noreply.github.com>

---
+++ 2788.go
@@ -0,0 +1,36 @@
+package dev
+
+import (
+	"megaCrawler/crawlers"
+	"megaCrawler/extractors"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func init() {
+	engine := crawlers.Register("2788", "GCF", "https://www.gcf.org.ph")
+
+	engine.SetStartingURLs([]string{"https://www.gcf.org.ph/sitemap.xml"})
+
+	extractorConfig := extractors.Config{
+		Author:       true,
+		Image:        false,
+		Language:     true,
+		PublishDate:  true,
+		Tags:         true,
+		Text:         false,
+		Title:        true,
+		TextLanguage: "",
+	}
+
+	extractorConfig.Apply(engine)
+
+	engine.OnXML("//loc", func(element *colly.XMLElement, ctx *crawlers.Context) {
+		engine.Visit(element.Text, crawlers.News)
+	})
+
+	engine.OnHTML("div > p", func(element *colly.HTMLElement, ctx *crawlers.Context) {
+		ctx.Content += element.Text
+	})
+
+}
